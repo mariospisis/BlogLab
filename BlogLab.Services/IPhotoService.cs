49218d55ef63d9fc464666d0bf6849f3d86a0a05
Marios Pisis <mariospisis@live.com>
2022-01-13 12:09:10 +0200

    Services Completed

---
+++ BlogLab.Services/IPhotoService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using CloudinaryDotNet.Actions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace BlogLab.Services
+{
+    public interface IPhotoService
+    {
+        public Task<ImageUploadResult> AddPhotoAsync(IFormFile file);
+
+        public Task<DeletionResult> DeletePhotoAsync(string publicId);
+    }
+}
--- BlogLab.Web/Startup.cs
+++ BlogLab.Web/Startup.cs
@@ -1,8 +1,10 @@
+using BlogLab.Models.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +14,17 @@ namespace BlogLab.Web
 {
     public class Startup
     {
+
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<CloudinaryOptions>(Configuration.GetSection("CloudinaryOptions"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
